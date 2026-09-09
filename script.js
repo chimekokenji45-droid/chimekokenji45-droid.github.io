27c2fd570a3fad99e50585990ec783f052508803
chimekokenji45-droid <chimekokenji45@gmail.com>
2026-09-09 11:10:22 +0100

    Fix formatting and update faucetEmail assignment

--- script.js
+++ script.js
@@ -554,12 +554,12 @@ function displayUser(user) {
   }
 
 
-    if (faucetEmail) {
+     if (faucetEmail) {
 
-    faucetEmail.textContent =
-      "";
+  faucetEmail.value =
+    user.email || "";
 
-    }
+     }
 
 
 
